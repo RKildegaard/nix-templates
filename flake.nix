4cda3ef42ac465d02623139c8266c71582588a72
RKildegaard <rasmuskildegaard20@gmail.com>
2026-02-09 16:26:46 +0100

    flake updated

--- flake.nix
+++ flake.nix
@@ -7,6 +7,18 @@
         path = ./python;
         description = "Python template";
       };
+      java-gradle = {
+        path = ./java/gradle;
+        description = "Java (Gradle) template";
+      };
+      java-maven = {
+        path = ./java/maven;
+        description = "Java (Maven) template";
+      };
+      java-javac = {
+        path = ./java/javac;
+        description = "Java (javac) template";
+      };
     };
   };
 }
