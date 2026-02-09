625b4f88767ac429f4b003482ee2865d9acd47b8
RKildegaard <rasmuskildegaard20@gmail.com>
2026-02-09 19:25:42 +0100

    made 2 python templates

---
+++ python/python312/flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Python devshell template";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        python = pkgs.python312;
+        pythonEnv = python.withPackages (ps: with ps; [
+          ipykernel
+          jupyter
+          notebook
+          jupyterlab
+          jupyterlab-lsp
+          jedi-language-server
+        ]);
+      in
+      {
+        formatter = pkgs.alejandra;
+
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pythonEnv
+            pkgs.uv
+            pkgs.ruff
+            pkgs.pyright
+            pkgs.pre-commit
+            pkgs.git
+          ];
+        };
+      }
+    );
+}
