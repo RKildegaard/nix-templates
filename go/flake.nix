d2da6bb8e0250cff0c1c476a2b985758c3b2b05c
RKildegaard <rasmuskildegaard20@gmail.com>
2026-02-09 19:49:02 +0100

    updated go flake

--- go/flake.nix
+++ go/flake.nix
@@ -10,37 +10,28 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-
-        # Use the default Go toolchain from nixpkgs (kept current/supported).
         go = pkgs.go;
       in
       {
         formatter = pkgs.alejandra;
 
         devShells.default = pkgs.mkShell {
-          packages = [
-            go
+          packages =
+            [
+              go
 
-            # Language server + debug
-            pkgs.gopls
-            pkgs.delve
+              # Language server + debug
+              pkgs.gopls
+              pkgs.delve
 
-            # Lint/format/test helpers
-            pkgs.golangci-lint
-            pkgs.gofumpt
-            pkgs.gotestsum
-            pkgs.staticcheck
+              # Lint/format/test helpers
+              pkgs.golangci-lint
+              pkgs.gofumpt
 
-            pkgs.git
-            pkgs.pre-commit
-          ];
-
-          shellHook = ''
-            echo "Entered Go devshell ($(${go}/bin/go version))"
-            if [ ! -f go.mod ]; then
-              echo "Tip: run 'go mod init <module>'"
-            fi
-          '';
+              pkgs.git
+              pkgs.pre-commit
+              pkgs.gotestsum
+            ]
         };
       }
     );
